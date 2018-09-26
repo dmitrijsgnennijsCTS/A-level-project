17ef20536f15af99b1351a05513970b95b0d65e2
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-09-26 20:56:53 +0100

    Fullscreen and exit on esc added

--- A Level project.py
+++ A Level project.py
@@ -1,8 +1,15 @@
-from kivy.app import App
+from kivy.app import App # Import the app to run the code and create window
+
+from kivy.uix.button import Button # Import button to use the buttons in code
+
+from kivy.config import Config # Import config to configure setting
+
+Config.set('kivy', 'exit_on_escape', '1') # When exit key pressed then close the program
+Config.set('graphics', 'fullscreen', 'auto') # Fulscreen is enabled and will be auto. So will be set to display res
 
 class MyApp(App):
 	def build(self):
-		pass
+		return Button(text = "button")
 
-if __name__ == "__main__":
-	MyApp().run()
+if __name__ == "__main__": # Check the name, if not a daughter program then run
+	MyApp().run() # Run the class
