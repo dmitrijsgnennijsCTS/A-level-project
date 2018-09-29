496b42e2eb4e15bb5e8368a2f0ce665364268d90
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-09-29 17:19:44 +0100

    All the main screen buttons have been created

--- A Level project.py
+++ A Level project.py
@@ -1,7 +1,7 @@
 from kivy.app import App # Import the app to run the code and create window
 
 from kivy.uix.button import Button # Import button to use the buttons in code
-from kivy.uix.anchorlayout import AnchorLayout # Allows the use of anchor layout
+from kivy.uix.floatlayout import FloatLayout
 
 from kivy.config import Config # Import config to configure setting
 
@@ -12,9 +12,15 @@ from kivy.core.window import Window # Import Window to get window size
 
 class MyApp(App):
 	def build(self):
-		
+		fl = FloatLayout()
+		fl.add_widget( Button(text = "Settings", size_hint = [.5,.1], pos = (200, 200)) )
+		fl.add_widget( Button(text = "Cruise Control : OFF", size_hint = [.5,.1], pos = (200, 400)) )
+		fl.add_widget( Button(text = "Lane Assist : OFF", size_hint = [.5,.1], pos = (200, 600)) )
+
 		print(str(Window.size[1]), str(Window.size[0])) # Gets the size of the display in pixels
-		return Button(text = "button1", size_hint = [.5,.1], pos = (int(Window.size[1]*.125), 0)) # Button that has relative size and position
+		#return Button(text = "button1", size_hint = [.5,.1], pos = (int(Window.size[1]*.125), 0)) # Button that has relative size and position
+
+		return fl
 
 if __name__ == "__main__": # Check the name, if not a daughter program then run
 	MyApp().run() # Run the class
