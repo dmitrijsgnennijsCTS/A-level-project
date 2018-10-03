1f60421c46c12c81539999a375c25d19af8a2b5b
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-10-03 09:32:50 +0100

    two screens in project app

--- Project file.py
+++ Project file.py
@@ -1,10 +1,44 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class MyApp(App):
+Builder.load_string("""
+<ScreenOne>:
+    FloatLayout:
+        Button:
+            text: "Go to screen 2"
+            on_press:
+                root.manager.transition.direction = "left"
+                root.manager.transition.duration = 1
+                root.manager.current = "screen_two"
+            size_hint: (.5, .5)
+
+<ScreenTwo>:
+    FloatLayout:
+        Button:
+            text: "Go to screen 1"
+            on_press:
+                root.manager.transition.direction = "right"
+                root.manager.transition.duration = 1
+                root.manager.current = "screen_one"
+            size_hint: (.5, .5)
+""")
+
+class ScreenOne(Screen):
+    pass
+
+class ScreenTwo(Screen):
+    pass
+
+screen_manager = ScreenManager()
+
+screen_manager.add_widget(ScreenOne(name = "screen_one"))
+screen_manager.add_widget(ScreenTwo(name = "screen_two"))
+
+class KivyTut2app(App):
+
     def build(self):
-        return Button(text = "test")
+        return screen_manager
 
-    
 if __name__ == "__main__":
-    MyApp().run()
+    KivyTut2app().run()
