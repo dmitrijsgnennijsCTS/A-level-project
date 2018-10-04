5bb0de2d53d14717aec2bf7d8a503746e0cc75ef
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-10-04 13:17:20 +0100

    Updated

--- Project file.py
+++ Project file.py
@@ -1,8 +1,21 @@
 from kivy.app import App
+from kivy.uix.label import Label
 
-class MainkvApp(App):
+
+class MainApp(App):
     def build(self):
-        pass
+        return Label()
     
 if __name__ == "__main__":
-    MainkvApp().run()
+    MainApp().run()
+
+
+##Button:
+##	text: "Cruise Control : OFF"
+##	size_hint: [.5, .1]
+##	pos_hint: {'x': 0, 'center_y': .05}
+##
+##Button:
+##	text: "Lane Assist : OFF"
+##	size_hint: [.5, .1]
+##	pos_hint: {'x': .5, 'center_y': .5}
