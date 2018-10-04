c24c4c22aa6964cf2c4c4eb05c0e981446a301cb
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-10-04 19:03:46 +0100

    Project file uses .kv to display Gui

--- Project file.py
+++ Project file.py
@@ -1,21 +1,35 @@
-from kivy.app import App
-from kivy.uix.label import Label
+from kivy.app import App # Import the app to run the code and create window
 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+from kivy.lang import Builder
+
+from kivy.config import Config # Import config to configure setting
+
+Config.set('kivy', 'exit_on_escape', '1') # When exit key pressed then close the program
+Config.set('graphics', 'fullscreen', 'auto') # Fulscreen is enabled and will be auto. So will be set to display res
+Config.set("graphics", "show_cursor", '1')
+
+
+from kivy.core.window import Window # Import Window to get window size
+
+Window.clearcolor = ((.2*.75), (.72*.75) ,(.80*.75) ,1) # Colour of the window
+
+class MainScreen(Screen, FloatLayout):
+	pass
+
+class SettingsScreen(Screen):
+	pass
+
+class ScreenManagement(ScreenManager):
+	pass
+
+gui = Builder.load_file('projectfile.kv')
 
 class MainApp(App):
-    def build(self):
-        return Label()
-    
+	def build(self):
+		return gui
+
 if __name__ == "__main__":
-    MainApp().run()
-
-
-##Button:
-##	text: "Cruise Control : OFF"
-##	size_hint: [.5, .1]
-##	pos_hint: {'x': 0, 'center_y': .05}
-##
-##Button:
-##	text: "Lane Assist : OFF"
-##	size_hint: [.5, .1]
-##	pos_hint: {'x': .5, 'center_y': .5}
+	MainApp().run()
