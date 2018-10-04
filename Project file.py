573970d1198ebf33092b021860a6ef4d5cfeb991
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-10-04 12:29:51 +0100

    Tried to create a program from a .kv file

--- Project file.py
+++ Project file.py
@@ -1,44 +1,8 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_string("""
-<ScreenOne>:
-    FloatLayout:
-        Button:
-            text: "Go to screen 2"
-            on_press:
-                root.manager.transition.direction = "left"
-                root.manager.transition.duration = 1
-                root.manager.current = "screen_two"
-            size_hint: (.5, .5)
-
-<ScreenTwo>:
-    FloatLayout:
-        Button:
-            text: "Go to screen 1"
-            on_press:
-                root.manager.transition.direction = "right"
-                root.manager.transition.duration = 1
-                root.manager.current = "screen_one"
-            size_hint: (.5, .5)
-""")
-
-class ScreenOne(Screen):
-    pass
-
-class ScreenTwo(Screen):
-    pass
-
-screen_manager = ScreenManager()
-
-screen_manager.add_widget(ScreenOne(name = "screen_one"))
-screen_manager.add_widget(ScreenTwo(name = "screen_two"))
-
-class KivyTut2app(App):
-
+class MainkvApp(App):
     def build(self):
-        return screen_manager
-
+        pass
+    
 if __name__ == "__main__":
-    KivyTut2app().run()
+    MainkvApp().run()
