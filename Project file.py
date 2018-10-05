6edeff6dd9357b24a6f4b16fc178fdaa8053376f
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-10-05 15:22:38 +0100

    Buttons in settings menu have basic functionality

--- Project file.py
+++ Project file.py
@@ -17,37 +17,64 @@ from kivy.core.window import Window # Import Window to get window size
 Window.clearcolor = ((.2*.75), (.72*.75) ,(.80*.75) ,1) # Colour of the window
 
 class MainScreen(Screen, FloatLayout):
-    def Settings_Button(self):
-        print("Settings pressed")
-
-
+    
     def Cruise_Control_Button(self):
-	#print("Cruise Control pressed")   
+        
         if self.btn_c.text == "Cruise Control: Off":
-            print(self.width)
             self.btn_c.text = ("Cruise Control: On")
         else:
             self.btn_c.text = ("Cruise Control: Off")
 
 
     def Lane_Assist_Button(self):
+        
         if self.btn_l.text == "Lane Assist : Off":
              self.btn_l.text = ("Lane Assist : On")
         else:
             self.btn_l.text = ("Lane Assist : Off")
+
+            
     pass
 
 class SettingsScreen(Screen, FloatLayout):
+    
     def Speed_Units(self):
-        print("Speed unit button pressed")
+        
+        if self.speedUnit.text == "Mph":
+             self.speedUnit.text = ("Kph")
+        else:
+            self.speedUnit.text = ("Mph")
+        
     def Pedestrians(self):
-        print("Pedestrian button pressed")
+        
+        if self.pedestrians.text == "Off":
+             self.pedestrians.text = ("On")
+        else:
+            self.pedestrians.text = ("Off")
+        
     def Distance_Units(self):
-        print("Distance units button pressed")
+        
+        if self.distanceUnit.text == "Meters":
+             self.distanceUnit.text = ("Yards")
+        elif self.distanceUnit.text == "Yards":
+            self.distanceUnit.text = ("Feet")
+        else:
+            self.distanceUnit.text = ("Meters")
+        
     def Distance_ToCar(self):
-        print("Distance to car button pressed")
+        
+        if self.carDistance.text == "Off":
+             self.carDistance.text = ("On")
+        else:
+            self.carDistance.text = ("Off")
+        
     def Car_Speed(self):
-        print("Speed of car in front button pressed")
+        
+        if self.carSpeed.text == "Off":
+             self.carSpeed.text = ("On")
+        else:
+            self.carSpeed.text = ("Off")
+        
     pass
 
 class ScreenManagement(ScreenManager):
