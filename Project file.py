d6f5c22fc8c4584ccfe264f652322f3baf141329
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-10-15 17:44:38 +0100

    update

--- Project file.py
+++ Project file.py
@@ -1,7 +1,7 @@
 from kivy.app import App # Import the app to run the code and create window
 
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.floatlayout import FloatLayout # Import the ability of putting widgets in any place on the window
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition # Import the ability to create multiple screens and anything related
 
 from kivy.lang import Builder
 
@@ -9,7 +9,7 @@ from kivy.config import Config # Import config to configure setting
 
 Config.set('kivy', 'exit_on_escape', '1') # When exit key pressed then close the program
 Config.set('graphics', 'fullscreen', 'auto') # Fulscreen is enabled and will be auto. So will be set to display res
-Config.set("graphics", "show_cursor", '1')
+Config.set("graphics", "show_cursor", '1') # Allow the cursor to be shown on the display when the program is running.
 
 
 from kivy.core.window import Window # Import Window to get window size
@@ -17,24 +17,28 @@ from kivy.core.window import Window # Import Window to get window size
 Window.clearcolor = ((.2*.75), (.72*.75) ,(.80*.75) ,1) # Colour of the window
 
 class MainScreen(Screen, FloatLayout):
-
-	def Settings_Button(self):
-		print("Settings pressed")
-
-
-	def Cruise_Control_Button(self):
-		print("Cruise Control pressed")
-		self.lbl.text = "Works"
+    def Settings_Button(self):
+        print("Settings pressed")
 
 
+    def Cruise_Control_Button(self):
+	#print("Cruise Control pressed")   
+        if self.btn_c.text == "Cruise Control: Off":
+            print(self.width)
+            self.btn_c.text = ("Cruise Control: On")
+        else:
+            self.btn_c.text = ("Cruise Control: Off")
 
-	def Lane_Assist_Button(self):
-		print("Lane Assist pressed")
 
-	pass
+    def Lane_Assist_Button(self):
+        if self.btn_l.text == "Lane Assist : Off":
+             self.btn_l.text = ("Lane Assist : On")
+        else:
+            self.btn_l.text = ("Lane Assist : Off")
+    pass
 
-class SettingsScreen(Screen):
-	pass
+class SettingsScreen(Screen, FloatLayout):
+    pass
 
 class ScreenManagement(ScreenManager):
 	pass
