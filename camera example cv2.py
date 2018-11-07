dfe7c584d5a06fd9e76300122334d3fd8212b670
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-11-07 22:46:31 +0000

    new opencv

--- camera example cv2.py
+++ camera example cv2.py
@@ -1,43 +1,33 @@
-from kivy.app import App
-from kivy.uix.image import Image
-from kivy.clock import Clock
-from kivy.graphics.texture import Texture
 import cv2
-
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.lang import Builder
-from kivy.core.window import Window
-
-
-class KivyCamera(Image):
-    def __init__(self, capture, fps, **kwargs):
-        super(KivyCamera, self).__init__(**kwargs)
-        self.capture = capture
-        Clock.schedule_interval(self.update, 1.0 / fps)
-
-    def update(self, dt):
-        ret, frame = self.capture.read()
-        if ret:
-            buf1 = cv2.flip(frame, 0)
-            buf = buf1.tostring()
-            image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')#(480,640)
-            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            # display image from the texture
-            self.texture = image_texture
-
-
-class CamApp(App):
-    def build(self):
-        self.camera= cv2.VideoCapture()
-        self.my_camera = KivyCamera(capture=self.camera, fps=10,resolution=(1280,960))
-        return self.my_camera
-
-    def on_stop(self):
-        self.camera.release()
-
-
-if __name__ == '__main__':
-    CamApp().run()
+import numpy as np
+ 
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
+ 
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream")
+ 
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+ 
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
+ 
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+ 
+  # Break the loop
+  else: 
+    break
+ 
+# When everything done, release the video capture object
+cap.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
