c03cdd2880c9ca93dbca1c1b6342d1d869a529f2
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-11-07 21:54:06 +0000

    New opencv example file

--- camera example cv2.py
+++ camera example cv2.py
@@ -1,9 +1,15 @@
-# coding:utf-8
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 import cv2
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.core.window import Window
 
 
 class KivyCamera(Image):
@@ -15,11 +21,9 @@ class KivyCamera(Image):
     def update(self, dt):
         ret, frame = self.capture.read()
         if ret:
-            # convert it to texture
             buf1 = cv2.flip(frame, 0)
             buf = buf1.tostring()
-            image_texture = Texture.create(
-                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')#(480,640)
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
@@ -27,13 +31,12 @@ class KivyCamera(Image):
 
 class CamApp(App):
     def build(self):
-        self.capture = cv2.VideoCapture(1)
-        self.my_camera = KivyCamera(capture=self.capture, fps=30)
+        self.camera= cv2.VideoCapture()
+        self.my_camera = KivyCamera(capture=self.camera, fps=10,resolution=(1280,960))
         return self.my_camera
 
     def on_stop(self):
-        #without this, app will not exit even if the window is closed
-        self.capture.release()
+        self.camera.release()
 
 
 if __name__ == '__main__':
