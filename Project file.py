240ebe34f904bda39b58212cfea0ba742af3c363
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-11-15 13:33:56 +0000

    picture showing incorrect

--- Project file.py
+++ Project file.py
@@ -1,8 +1,6 @@
 from kivy.app import App # Import the app to run the code and create window
 from kivy.uix.floatlayout import FloatLayout # Import the ability of putting widgets in any place on the window
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition # Import the ability to create multiple screens and anything related
-from kivy.graphics.texture import Texture
-from kivy.graphics.texture import *
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 
@@ -13,36 +11,47 @@ import numpy as np
 import cv2
 import PIL
 from kivy.uix.image import Image
-from kivy.core.image import Image as CoreImage
 from kivy.clock import Clock
 
 Config.set('kivy', 'exit_on_escape', '1') # When exit key pressed then close the program
-Config.set('graphics', 'fullscreen', '0') # Fulscreen is enabled and will be auto. So will be set to display res
+Config.set('graphics', 'fullscreen', 'auto') # Fulscreen is enabled and will be auto. So will be set to display res
 Config.set("graphics", "show_cursor", '1') # Allow the cursor to be shown on the display when the program is running.
 
 
 from kivy.core.window import Window # Import Window to get window size
 
 Window.clearcolor = ((.2*.75), (.72*.75) ,(.80*.75) ,1) # Colour of the window
+source = ''
 
-class MainScreen(Screen, FloatLayout):
-    
-    def Cruise_Control_Button(self):
-        
-        if self.btn_c.text == "Cruise Control: Off":
-            self.btn_c.text = ("Cruise Control: On")
-        else:
-            self.btn_c.text = ("Cruise Control: Off")
+class MainScreen(Screen, FloatLayout, Image):
+	def __init__(self, **kwargs):
+		super(MainScreen, self).__init__(**kwargs)
+		self.source = 'img.jpg' #Getting two images
+	# def __init__(self, capture, fps, **kwargs): #Error whilst running here
+	# 	print("here 2")
+	# 	super(MainScreen, self).__init__(**kwargs)
+	# 	self.capture = capture
+	# 	Clock.schedule_interval(self.update, 1.0 / fps)
 
+	# def update(self, dt):
+	# 	ret, frame = self.capture.read()
+	# 	#print(frame)
+	# 	imag = PIL.Image.fromarray(frame)
+	# 	imag.save('out.png')
+	# 	self.source = 'img.jpg'
 
-    def Lane_Assist_Button(self):
-        
-        if self.btn_l.text == "Lane Assist : Off":
-             self.btn_l.text = ("Lane Assist : On")
-        else:
-            self.btn_l.text = ("Lane Assist : Off")
-     
-    pass
+	def Cruise_Control_Button(self):
+		if self.btn_c.text == "Cruise Control: Off":
+			self.btn_c.text = ("Cruise Control: On")
+		else:
+			self.btn_c.text = ("Cruise Control: Off")
+
+	def Lane_Assist_Button(self):
+		if self.btn_l.text == "Lane Assist : Off":
+			self.btn_l.text = ("Lane Assist : On")
+		else:
+			self.btn_l.text = ("Lane Assist : Off")
+	pass
 
 class SettingsScreen(Screen, FloatLayout):
     
@@ -89,34 +98,32 @@ class SettingsScreen(Screen, FloatLayout):
 class ScreenManagement(ScreenManager):
 	pass
 
-class MyWidget(Widget):
-    pass
+# class MyWidget(Widget):
+#     pass
 
-class CameraCapture(Image):
-    def __init__(self, capture, fps, **kwargs):
-    	super(CameraCapture, self).__init__(**kwargs)
-    	self.capture = capture
-    	Clock.schedule_interval(self.update, 1.0 / fps)
+#class CameraCapture(Image):
+ #   def __init__(self, capture, fps, **kwargs):
+  #  	super(CameraCapture, self).__init__(**kwargs)
+   # 	self.capture = capture
+    #	Clock.schedule_interval(self.update, 1.0 / fps)
 
-    def update(self, dt):
-    	ret, frame = self.capture.read()
-    	#print(frame)
-    	imag = PIL.Image.fromarray(frame)
-    	imag.save('out.png')
-    	self.source = 'image.png'
+#    def update(self, dt):
+ #   	ret, frame = self.capture.read()
+  #  	#print(frame)
+   # 	imag = PIL.Image.fromarray(frame)
+    #	imag.save('out.png')
+    #	self.source = 'img.jpg'
 
 
 gui = Builder.load_file('projectfile.kv')
 
 class MainApp(App):
     def build(self):
-    	root = self.root
-    	self.capture = cv2.VideoCapture(0)
-    	self.my_camera = CameraCapture(capture = self.capture, fps = 0.1)
+    	#self.capture = cv2.VideoCapture(0)
     	return gui
 
-    def on_stop(self):
-        self.capture.release()
+    # def on_stop(self):
+    #     self.capture.release()
 
 if __name__ == "__main__":
 	MainApp().run()
