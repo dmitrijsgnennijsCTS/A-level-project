344e4ccc07210e85032730e782233a9821a01071
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-11-16 09:59:19 +0000

    Image is correctly showing, need to display the live feed now

--- Project file.py
+++ Project file.py
@@ -13,6 +13,9 @@ import PIL
 from kivy.uix.image import Image
 from kivy.clock import Clock
 
+from os.path import join, exists
+import os
+
 Config.set('kivy', 'exit_on_escape', '1') # When exit key pressed then close the program
 Config.set('graphics', 'fullscreen', 'auto') # Fulscreen is enabled and will be auto. So will be set to display res
 Config.set("graphics", "show_cursor", '1') # Allow the cursor to be shown on the display when the program is running.
@@ -21,12 +24,11 @@ Config.set("graphics", "show_cursor", '1') # Allow the cursor to be shown on the
 from kivy.core.window import Window # Import Window to get window size
 
 Window.clearcolor = ((.2*.75), (.72*.75) ,(.80*.75) ,1) # Colour of the window
-source = ''
 
-class MainScreen(Screen, FloatLayout, Image):
+class MainScreen(Screen, FloatLayout):
 	def __init__(self, **kwargs):
 		super(MainScreen, self).__init__(**kwargs)
-		self.source = 'img.jpg' #Getting two images
+		self.source ='img.jpg'
 	# def __init__(self, capture, fps, **kwargs): #Error whilst running here
 	# 	print("here 2")
 	# 	super(MainScreen, self).__init__(**kwargs)
