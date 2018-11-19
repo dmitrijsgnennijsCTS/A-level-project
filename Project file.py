ad0bb34024a308f316cb7d685b82b2bdd11789b9
Dmitrijs Gnennijs <32061@corbytechnicalschool.org>
2018-11-19 20:33:15 +0000

    the save image is wellllll slow and dont work, pointless

--- Project file.py
+++ Project file.py
@@ -12,10 +12,11 @@ import cv2
 import PIL
 from kivy.uix.image import Image
 from kivy.clock import Clock
+from time import time
 
 
 Config.set('kivy', 'exit_on_escape', '1') # When exit key pressed then close the program
-Config.set('graphics', 'fullscreen', 'auto') # Fulscreen is enabled and will be auto. So will be set to display res
+Config.set('graphics', 'fullscreen', '0') # Fulscreen is enabled and will be auto. So will be set to display res
 Config.set("graphics", "show_cursor", '1') # Allow the cursor to be shown on the display when the program is running.
 
 
@@ -24,11 +25,7 @@ from kivy.core.window import Window # Import Window to get window size
 Window.clearcolor = ((.2*.75), (.72*.75) ,(.80*.75) ,1) # Colour of the window
 
 class MainScreen(Screen, FloatLayout):
-	# def __init__(self, **kwargs):
-	# 	super(MainScreen, self).__init__(**kwargs)
-	# 	self.source ='img.jpg'
 	def __init__(self, **kwargs):
-		#print("here 2")
 		super(MainScreen, self).__init__(**kwargs)
 		self.capture = cv2.VideoCapture(0)
 		self.source = 'img.jpg'
@@ -36,18 +33,16 @@ class MainScreen(Screen, FloatLayout):
 
 	def update(self, dt):
 		ret, frame = self.capture.read()
-		print(frame)
-		print(ret)
-		self.ids.img.source = 'img2.jpg'
-		# if ret == True:
-		# 	print("I'm getting here")
-		# 	self.source = PIL.Image.fromarray(frame)
-		# else:
-		# 	print("here again")
-		# 	self.source = 'img.jpg'
-		# 	print("------------------No feed------------------")
-		# imag.save('out.png')
-		# self.source = 'img.jpg'
+		frame = PIL.Image.fromarray(frame)
+		frame.save('frame.jpg')
+		self.ids.img.source = 'frame.jpg'
+		print("change image")
+		if ret == True:
+			print("I'm getting here")
+		else:
+			print("here again")
+			self.ids.img.source = 'img.jpg'
+			print("------------------No feed------------------")
 
 	def on_stop(self):
 		self.capture.release()
@@ -110,33 +105,12 @@ class SettingsScreen(Screen, FloatLayout):
 class ScreenManagement(ScreenManager):
 	pass
 
-# class MyWidget(Widget):
-#     pass
-
-#class CameraCapture(Image):
- #   def __init__(self, capture, fps, **kwargs):
-  #  	super(CameraCapture, self).__init__(**kwargs)
-   # 	self.capture = capture
-    #	Clock.schedule_interval(self.update, 1.0 / fps)
-
-#    def update(self, dt):
- #   	ret, frame = self.capture.read()
-  #  	#print(frame)
-   # 	imag = PIL.Image.fromarray(frame)
-    #	imag.save('out.png')
-    #	self.source = 'img.jpg'
-
 
 gui = Builder.load_file('projectfile.kv')
 
 class MainApp(App):
     def build(self):
-    	# self.capture = cv2.VideoCapture(0)
-    	# MainScreen(capture=self.capture, fps=30)
     	return gui
-
-    # def on_stop(self):
-    #     self.capture.release()
 
 if __name__ == "__main__":
 	MainApp().run()
