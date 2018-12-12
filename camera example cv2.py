d378f11d9d84a7f0693f616cf3f076cd67c3865d
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2018-12-12 19:42:49 +0000

    V 1.0.2 - The haar cascade for stop is working well

--- camera example cv2.py
+++ camera example cv2.py
@@ -2,13 +2,21 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+haar_cascade = cv2.CascadeClassifier('C:/Users/Dmitrijs/Documents/GitHub/A-level-project-/stop/classifier/cascade.xml')
+
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5); 
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display the resulting frame
     cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) == 27:
         break
 
 # When everything done, release the capture
