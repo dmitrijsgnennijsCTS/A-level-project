6dacce2862277a3532a648f8bbee5aea50976efe
Dmitrijs <32061@corbytechnicalschool.org>
2019-02-07 08:40:31 +0000

    Added a test video

--- camera example cv2.py
+++ camera example cv2.py
@@ -1,34 +1,20 @@
 import numpy as np
+import numexpr as ne # library numexpr
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('VID_20181222_210041.mp4')
 haar_cascade = cv2.CascadeClassifier('stop/classifier/cascade.xml')
 
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    ##print("repeat")
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    s = 0
-    #print(len(gray))
-
-    for i in range(len(gray)):
-        #print('r',gray[i,0])
-        #print('g',gray[i,1])
-        #print('b',gray[i,2])
-        s = s + gray[i,0] + gray[i,1] + gray[i,2]
-
-    
-    s = int(s/(len(gray)*3))
-
-    print(s)
-
-    if s <= 80:
-        print('dark')
-    #print(gray)
-
+    a2D = frame.reshape(-1, frame.shape[-1])
+    col_range= (256, 256, 256)
+    eval_params = {'a0':a2D[:,0], 'a1':a2D[:,1], 'a2':a2D[:,2], 's0': col_range[0], 's1': col_range[1]}
+    a1D = ne.evaluate('a0*s0*s1+a1*s0+a2', eval_params)
+    print(np.unravel_index(np.bincount(a1D).argmax(), col_range))
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == 27:
         break
