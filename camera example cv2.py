7fad93118b7beea4c1fef6a339cd7faf585fda27
Dmitrijs <32061@corbytechnicalschool.org>
2019-02-08 09:46:05 +0000

    V 1.0.3 - Adding all pixel values is way too long

--- camera example cv2.py
+++ camera example cv2.py
@@ -1,24 +1,32 @@
 import numpy as np
 import numexpr as ne # library numexpr
 import cv2
+from PIL import Image
 
-cap = cv2.VideoCapture('VID_20181222_210041.mp4')
-haar_cascade = cv2.CascadeClassifier('stop/classifier/cascade.xml')
+cap = cv2.VideoCapture(0)
 
+#cap = cv2.VideoCapture('VID_20181222_210041.mp4')
+#haar_cascade = cv2.CascadeClassifier('stop/classifier/cascade.xml')
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    a2D = frame.reshape(-1, frame.shape[-1])
-    col_range= (256, 256, 256)
-    eval_params = {'a0':a2D[:,0], 'a1':a2D[:,1], 'a2':a2D[:,2], 's0': col_range[0], 's1': col_range[1]}
-    a1D = ne.evaluate('a0*s0*s1+a1*s0+a2', eval_params)
-    print(np.unravel_index(np.bincount(a1D).argmax(), col_range))
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == 27:
-        break
+ret, frame = cap.read()
+cv2.imshow('frame2', frame)
+#print(frame)
+calc = 0
+for i in range(len(frame)):
+    for n in range(len(frame[i])):
+        for x in range(len(frame[i][n])):
+            calc += frame[i][n][x]
+        #print(frame[i][n])
+#frame.resize((1,1))
+#print(frame) # Prints the mean colour of the image somehow
+#cv2.imshow('frame', frame)
+#if cv2.waitKey(1) == 27:
+ #   break
 
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+print(int(calc/(3*(len(frame)*len(frame[0])))))
+frame.resize((1,1))
+print(frame[0][0]) # Prints the mean colour 
+#print(len(frame[0]))
+#cap.release()
+#cv2.destroyAllWindows()
