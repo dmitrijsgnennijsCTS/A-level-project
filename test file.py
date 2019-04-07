b36d0b6143df74982641435a59fec0116fc6f706
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2019-04-07 14:36:02 +0100

    V 1.0.3 - Canny edge detection added

--- test file.py
+++ test file.py
@@ -3,14 +3,21 @@ from PIL import ImageGrab
 import cv2
 import time
 
+def process_img(original_image):
+
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
+
 def screen_record(): 
-    last_time = time.time()
+    #last_time = time.time()
     while(True):
         # 800x600 windowed mode
         printscreen =  np.array(ImageGrab.grab(bbox=(0,95,1024,730)))
-        print('loop took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        new_screen = process_img(printscreen)
+        #print('loop took {} seconds'.format(time.time()-last_time))
+        #last_time = time.time()
+        cv2.imshow('window',new_screen)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
