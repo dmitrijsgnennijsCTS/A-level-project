db94013b9fd0e6c0bfc1515a1475da7af1166f5a
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2019-04-07 19:29:48 +0100

    V 1.0.3 - hough line feature added

--- test file.py
+++ test file.py
@@ -3,15 +3,36 @@ from PIL import ImageGrab
 import cv2
 import time
 import pyautogui
-from directkeys import PressKey, W, A, S, D
+from directkeys import ReleaseKey, PressKey, W, A, S, D
 
-def process_img(image):
-    original_image = image
-    # convert to gray
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # edge detection
-    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+    except:
+        pass
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=300, threshold2=400)
+    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
+    vertices = np.array([[0,760],[0,600], [312,365], [712,365], [1024,600], [1024,760]], np.int32)
+    processed_img = roi(processed_img, [vertices])
+
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
+    draw_lines(processed_img, lines)
     return processed_img
+
 
 def main():
     
@@ -21,8 +42,8 @@ def main():
 
     last_time = time.time()
     while True:
-        PressKey(W)
-        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        #PressKey(W)
+        screen =  np.array(ImageGrab.grab(bbox=(0,35,1024,800)))
         #print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         new_screen = process_img(screen)
