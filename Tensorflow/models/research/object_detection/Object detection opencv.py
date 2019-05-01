558bbf72402a806ce4482ce9c72c114bfd50b3a7
Dmitrijs Gnennijs <dimagnennijs@gmail.com>
2019-05-01 18:04:17 +0100

    V 1.1.0 - tensorflow works/object identification

--- Tensorflow/models/research/object_detection/Object detection opencv.py
+++ Tensorflow/models/research/object_detection/Object detection opencv.py
@@ -1,7 +1,3 @@
-# coding: utf-8
-# # Object Detection Demo
-# License: Apache License 2.0 (https://github.com/tensorflow/models/blob/master/LICENSE)
-# source: https://github.com/tensorflow/models
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -14,22 +10,34 @@ from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
-from grabscreen import grab_screen
+
 import cv2
+cap = cv2.VideoCapture(0)
 
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
-#
-os.chdir( 'D:\\tnsrflw\\models\\research\\object_detection' )
+
+
 # ## Object detection imports
 # Here are the imports from the object detection module.
 
-from object_detection.utils import label_map_util
+# In[3]:
 
-from object_detection.utils import visualization_utils as vis_util
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
 
 
 # # Model preparation 
+
+# ## Variables
+# 
+# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
+# 
+# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+
+# In[4]:
+
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
@@ -43,7 +51,11 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
+
 # ## Download Model
+
+# In[5]:
+
 opener = urllib.request.URLopener()
 opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
 tar_file = tarfile.open(MODEL_FILE)
@@ -54,6 +66,9 @@ for file in tar_file.getmembers():
 
 
 # ## Load a (frozen) Tensorflow model into memory.
+
+# In[6]:
+
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
@@ -65,26 +80,45 @@ with detection_graph.as_default():
 
 # ## Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+# In[7]:
+
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 
 # ## Helper code
+
+# In[8]:
+
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
+
+# # Detection
+
+# In[9]:
+
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+
 # Size, in inches, of the output images.
 IMAGE_SIZE = (12, 8)
+
+
+# In[10]:
 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     while True:
-      #screen = cv2.resize(grab_screen(region=(0,40,1280,745)), (WIDTH,HEIGHT))
-      screen = cv2.resize(grab_screen(region=(0,40,1280,745)), (800,450))
-      image_np = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
+      ret, image_np = cap.read()
       # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)
       image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -109,22 +143,7 @@ with detection_graph.as_default():
           use_normalized_coordinates=True,
           line_thickness=8)
 
-
-      for i,b in enumerate(boxes[0]):
-        #                 car                    bus                  truck
-        if classes[0][i] == 3 or classes[0][i] == 6 or classes[0][i] == 8:
-          if scores[0][i] >= 0.5:
-            mid_x = (boxes[0][i][1]+boxes[0][i][3])/2
-            mid_y = (boxes[0][i][0]+boxes[0][i][2])/2
-            apx_distance = round(((1 - (boxes[0][i][3] - boxes[0][i][1]))**4),1)
-            cv2.putText(image_np, '{}'.format(apx_distance), (int(mid_x*800),int(mid_y*450)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
-
-            if apx_distance <=0.5:
-              if mid_x > 0.3 and mid_x < 0.7:
-                cv2.putText(image_np, 'WARNING!!!', (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 3)
-
-
-      cv2.imshow('window',cv2.resize(image_np,(800,450)))
+      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
       if cv2.waitKey(25) & 0xFF == ord('q'):
-          cv2.destroyAllWindows()
-          break
+        cv2.destroyAllWindows()
+        break
